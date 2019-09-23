f435353ee623817de288a07aee98021de59495ff
mpfullstack <info@marcperez.cat>
2019-09-23 16:38:41 +0200

    Convert markdown project content field to html

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,14 +1,18 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+
+const MarkdownIt = require('markdown-it');
+const md = new MarkdownIt();
+
 export default ({ data }) => {
   const project = data.allStrapiProject.edges[0].node;
-  console.log('project', project);
   return (
     <Layout>
       <div>
         <h1>{project.title}</h1>
-        <p>{project.content}</p>
+        <div
+          dangerouslySetInnerHTML={{ __html: md.render(project.content) }} />
       </div>
     </Layout>
   )
