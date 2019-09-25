609ba071c0c8b03348b05d1d5da47bdfddceb3eb
mpfullstack <info@marcperez.cat>
2019-09-25 18:57:33 +0200

    Added baseUrl in cypress.json

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,6 +1,6 @@
 describe('Home page content', () => {
   it('Should have a list of projects with at least one project', () => {
-    cy.visit('http://localhost:8000');
+    cy.visit('/');
     cy.get('.projects-list')
       .find('.projects-list__item')
       .its('length')
