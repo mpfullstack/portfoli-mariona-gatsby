6c2639ca8e1798c120f907eef4c486867843ce4c
mpfullstack <info@marcperez.cat>
2020-01-06 09:10:44 +0100

    Revert last commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: 'http://mariona.h.marcperez.cat',
+        apiURL: process.env.GATSBY_STRAPI_URL_API,
         contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
           `project`,
           `page`,
