743eb55813f3dd88b7a26ac82a0a8df5445e866d
mpfullstack <info@marcperez.cat>
2020-11-01 08:46:49 +0100

    Changed envar path in deploy script

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,19 +1,24 @@
-cd $SRC_CODE/site
+cd $GATSBY_SRC_CODE_PATH
 git pull --all
 npm install
 
+if [ ! -d $GATSBY_SRC_APP_PATH ]; then
+  mkdir $GATSBY_SRC_APP_PATH
+fi
+
 # TODO: Only run tests in development and staging environments
 
-gatsby clean
-gatsby build
+./node_modules/.bin/gatsby clean
+./node_modules/.bin/gatsby build
 
-# Remove SRC_CODE full path from js files
-find "$SRC_CODE/site/public" -type f -name '*.js' -exec sed -i 's@'"$SRC_CODE/site/public"'@''@g' {} \;
-find "$SRC_CODE/site/public" -type f -name '*.js' -exec sed -i 's@'"$SRC_CODE/site"'@''@g' {} \;
+# Remove GATSBY_SRC_CODE_PATH full path from js files
+# TODO: Review if necessary
+find "$GATSBY_SRC_CODE_PATH/public" -type f -name '*.js' -exec sed -i 's@'"$GATSBY_SRC_CODE_PATH/public"'@''@g' {} \;
+find "$GATSBY_SRC_CODE_PATH/public" -type f -name '*.js' -exec sed -i 's@'"$GATSBY_SRC_CODE_PATH"'@''@g' {} \;
 
 current=`date "+%Y%m%d_%s"`
-rsync -ah $SRC_CODE/site/public/* $SRC_APP/site/$current
+rsync -ah $GATSBY_SRC_CODE_PATH/public/* $GATSBY_SRC_APP_PATH/$current
 
 # Make new symlink
-rm $SRC_APP/site/current
-ln -s $SRC_APP/site/$current $SRC_APP/site/current
+rm $GATSBY_SRC_APP_PATH/current
+ln -s $GATSBY_SRC_APP_PATH/$current $GATSBY_SRC_APP_PATH/current
