07724972851751217507f79807dd97159d3c10c1
mpfullstack <info@marcperez.cat>
2020-11-02 17:45:09 +0100

    Styles adjustments

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,67 +11,69 @@ const MarkdownIt = require('markdown-it');
 const md = new MarkdownIt();
 
 const AboutPageWrapper = styled.div`
-  display: flex;
-  position: relative;
-  .background-image {
-    width: 100%;
-    max-width: 500px;
-    position: absolute;
-    top: 25%;
-    left: 0;
-    background-color: #E8F5E0;
-    height: 450px;
-    z-index: 0;
-    opacity: .6;
-  }
-  .image-container {
-    position: relative;
-    z-index: 1;
-    width: 30%;
-    margin-left: 20px;
-  }
-  .text-container {
-    position: relative;
-    z-index: 1;
-    width: 60%;
-    margin-left: auto;
-    margin-top: 80px;
-    .introduction {
-      font-size: 32px;
-      line-height: 1.5;
-    }
-    .content {
-      p {
-        font-size: 16px;
-      }
-    }
-    .cv {
-      font-size: 16px;
-      a {
-        color: ${theme.textColor};
-        text-transform: uppercase;
-        font-weight: 800;
-      }
-    }
-  }
-  @media only screen and (max-width: ${theme.SIZES.M}) {
-    width: 90%;
-    margin: 70px auto 0;
+  .about-container {
     display: flex;
-    flex-direction: column;
+    position: relative;
     .background-image {
-      top: 8%;
-      height: 340px;
+      width: 100%;
+      max-width: 500px;
+      position: absolute;
+      top: 25%;
+      left: 0;
+      background-color: #E8F5E0;
+      height: 450px;
+      z-index: 0;
+      opacity: .6;
     }
     .image-container {
-      width: 70%;
-      position: inherit;
-      margin: 0 0 0 20px;
+      position: relative;
+      z-index: 1;
+      width: 30%;
+      margin-left: 20px;
     }
     .text-container {
-      position: inherit;
-      width: 100%;
-      margin: 80px 0 0 0;
+      position: relative;
+      z-index: 1;
+      width: 60%;
+      margin-left: auto;
+      margin-top: 80px;
+      .introduction {
+        font-size: 32px;
+        line-height: 1.5;
+      }
+      .content {
+        p {
+          font-size: 16px;
+        }
+      }
+      .cv {
+        font-size: 16px;
+        a {
+          color: ${theme.textColor};
+          text-transform: uppercase;
+          font-weight: 800;
+        }
+      }
+    }
+    @media only screen and (max-width: ${theme.SIZES.M}) {
+      width: 90%;
+      margin: 70px auto 0;
+      display: flex;
+      flex-direction: column;
+      .background-image {
+        top: 8%;
+        height: 340px;
+      }
+      .image-container {
+        width: 70%;
+        position: inherit;
+        margin: 0 0 0 20px;
+      }
+      .text-container {
+        position: inherit;
+        width: 100%;
+        margin: 80px 0 0 0;
+      }
     }
   }
   .ScrollbarsCustom-Wrapper {
@@ -92,15 +94,17 @@ const AboutPage = ({ location, data }) => {
     <Layout location={location}>
       <SEO title="Portfolio Mariona Mercadal" />
       <AboutPageWrapper>
-        <Scrollbar style={{ height: '90vh'}}>
-          <div className='background-image'></div>
-          <div className='image-container'>
-            <Img fluid={image.childImageSharp.fluid} />
-          </div>
-          <div className='text-container'>
-            <div className='introduction' dangerouslySetInnerHTML={{ __html: md.render(introduction) }}/>
-            <div className='content' dangerouslySetInnerHTML={{ __html: md.render(content) }}/>
-            <p className='cv'><a href={cv.publicURL}>Download CV</a></p>
+        <Scrollbar style={{ height: '100vh'}}>
+          <div className='about-container'>
+            <div className='background-image'></div>
+            <div className='image-container'>
+              <Img fluid={image.childImageSharp.fluid} />
+            </div>
+            <div className='text-container'>
+              <div className='introduction' dangerouslySetInnerHTML={{ __html: md.render(introduction) }}/>
+              <div className='content' dangerouslySetInnerHTML={{ __html: md.render(content) }}/>
+              <p className='cv'><a href={cv.publicURL}>Download CV</a></p>
+            </div>
           </div>
         </Scrollbar>
       </AboutPageWrapper>
